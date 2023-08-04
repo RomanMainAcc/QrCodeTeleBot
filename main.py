f5943a65eaf9e7d51fe5a18ff3ea571972fc20c5
RomanMainAcc <romanborisuk0@gmail.com>
2023-08-04 20:16:56 +0300

    refactor

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
 import telebot
 import pyqrcode
+import config
 from PIL import Image
 from pyzbar.pyzbar import decode
+import requests
+from io import BytesIO
 
-bot = telebot.TeleBot('6046831990:AAGMjVSRxnL_WX9IdfVcMLgjyoUcIjmSBYg')
-
-qr_code_filename = 'qr_code.png'
+bot = telebot.TeleBot(config.TOKEN)
 
 
-def download_file(file_id: str) -> None:
+def download_file_to_pillow(file_id: str) -> Image.Image:
     qr_code_info = bot.get_file(file_id)
-    qr_code_file = bot.download_file(qr_code_info.file_path)
+    qr_code_url = f"https://api.telegram.org/file/bot{config.TOKEN}/{qr_code_info.file_path}"
 
-    with open(qr_code_filename, 'wb') as file:
-        file.write(qr_code_file)
+    response = requests.get(qr_code_url)
+    response.raise_for_status()
+
+    buffer = BytesIO(response.content)
+
+    return Image.open(buffer)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def starter(message):
+def starter(message: telebot.types.Message) -> None:
     bot.send_message(message.chat.id, "Hello, send me the text and I will generate a qr code")
 
 
 @bot.message_handler(content_types=['text'])
-def text_to_qr(message):
+def text_to_qr(message: telebot.types.Message) -> None:
     qr_code = pyqrcode.create(message.text)
-    qr_code.png(qr_code_filename, scale=6)
-
-    with open(qr_code_filename, 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
+    buffer = BytesIO()
+    qr_code.png(buffer, scale=6)
+    buffer.seek(0)
+    bot.send_photo(message.chat.id, photo=buffer)
 
 
 @bot.message_handler(content_types=['photo'])
-def qr_to_text(message):
+def qr_to_text(message: telebot.types.Message) -> None:
     photo_id = message.photo[-1].file_id
-    download_file(photo_id)
+    qr_code_img = download_file_to_pillow(photo_id)
 
-    decoded_objects = decode(Image.open(qr_code_filename))
+    decoded_objects = decode(qr_code_img)
 
     if decoded_objects:
         qr_text = decoded_objects[0].data.decode('utf-8')
@@ -44,4 +49,5 @@ def qr_to_text(message):
         bot.reply_to(message, "QR code was not recognized or image not found.")
 
 
-bot.polling(none_stop=True)
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
